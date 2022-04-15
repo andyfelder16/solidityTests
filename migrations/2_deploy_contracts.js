e93fd5154dc119b5a43cbf5fb08ce569ff361a15
Andres Felder <andyfelder16@gmail.com>
2022-04-15 20:52:54 -0300

    volvi al estado con primeros ejercicios

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,9 @@ const testContract = artifacts.require("./contracts/testContract.sol");
 const exerciseOne = artifacts.require("./contracts/exerciseOne.sol");
 const exerciseTwo = artifacts.require("./contracts/exerciseTwo.sol");
 
-
-
 module.exports = function (deployer) {
   deployer.deploy(testContract);
   deployer.deploy(exerciseOne);
   deployer.deploy(exerciseTwo);
+
 };
